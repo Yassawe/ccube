6277618ea7e5798ddb5428672776f5dc65441087
Yassawe <jassaui.k@gmail.com>
2022-02-21 11:15:04 +0900

    minor changes

--- ccube.cu
+++ ccube.cu
@@ -20,11 +20,11 @@ struct Node {
     cudaStream_t R_stream; 
     cudaStream_t B_stream;
 
-    int num_c;
-    int c[2]; //[left, right] order
-    int p;
+    int parent;
+    int left; //order [left, right].
+    int right;
     
-    float *buffer; // do i decide on simultaneous send-recieve model? NO!!!
+    float *buffer; 
 }
 
 struct Node tree[4];
@@ -44,33 +44,58 @@ void createCommunicator(){
     cudaDeviceEnablePeerAccess(2,0);
     cudaStreamCreateWithFlags(&(tree[0].R_stream), cudaStreamNonBlocking);
     cudaStreamCreateWithFlags(&(tree[0].B_stream), cudaStreamNonBlocking);
-    tree[0].num_c = 1;
-    tree[0].c[0] = 2;
-    tree[0].p  = -1;
+    tree[0].left = 2;
+    tree[0].right = -1;
+    tree[0].parent  = -1;
 
     cudaSetDevice(1);
     cudaDeviceEnablePeerAccess(2,0);
     cudaStreamCreateWithFlags(&(tree[1].R_stream), cudaStreamNonBlocking);
     cudaStreamCreateWithFlags(&(tree[1].B_stream), cudaStreamNonBlocking);
-    tree[1].num_c = 0;
-    tree[1].p = 2;
+    tree[1].left = -1;
+    tree[1].right = -1;
+    tree[1].parent = 2;
 
     cudaSetDevice(2);
     cudaDeviceEnablePeerAccess(1,0);
     cudaDeviceEnablePeerAccess(3,0);
     cudaStreamCreateWithFlags(&(tree[2].R_stream), cudaStreamNonBlocking);
     cudaStreamCreateWithFlags(&(tree[2].B_stream), cudaStreamNonBlocking);
-    tree[2].num_c = 2;
-    tree[2].c[0] = 1;
-    tree[2].c[1] = 3;
+    tree[2].left = 1;
+    tree[2].right = 3;
+    tree[2].parent = 0;
     
     cudaSetDevice(3);
     cudaDeviceEnablePeerAccess(2,0);
     cudaStreamCreateWithFlags(&(tree[3].R_stream), cudaStreamNonBlocking);
     cudaStreamCreateWithFlags(&(tree[3].B_stream), cudaStreamNonBlocking);
-    tree[3].num_c = 0;
-    tree[3].p = 2;
+    tree[3].left = -1;
+    tree[3].right = -1;
+    tree[3].parent = 2;
 }
 
 
-// adapt the 2 port simultaneous send-recieve model.
+
+
+void allreduce(void* sendbuff, void* recvbuff, int count, int chunk_size){
+    // multiprocess function
+    // create n threads, each launching reduce_kernel and broadcast_kernel on every device
+    // using tree struct
+
+}
+
+
+// adapt the C-Cube under the 3 port simultaneous send-recieve model.
+// two streams: reduce and broadcast
+// tree should be a structure visible and referenceable from the device, not only host controlled
+
+__global__ void reduce_kernel(float* self_buff, float* parent_buff, float* left_buff, float* right_buff, int chunksize){ //make it agnostic to where it runs
+
+
+}
+
+__global__ void broadcast_kernel(float* self_buff, float* parent_buff, float* left_buff, float* right_buff, int chunksize){
+
+
+}
+

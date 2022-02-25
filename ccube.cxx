31052dfa6dcdfd6204ad7314837bce5c9004ad05
Yassawe <jassaui.k@gmail.com>
2022-02-25 18:00:28 +0900

    kernels done

--- ccube.cxx
+++ ccube.cxx
@@ -3,10 +3,11 @@
 
 
 int main(){
-    //create and allocate some data to be allreduced
-    // create n threads, each calling allreduce() on its device
     struct Node tree[4];
 
     createCommunicator(tree);
-    
+    //TODO: 
+    //1) create N threads, each allocating dummy data and launching allreduce() on its device
+    //2) write tests to check functional corectness
+    //3) record time performance
 }
